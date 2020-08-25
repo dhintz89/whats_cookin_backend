18bd0910dbd145735f1f2d899b97c15f631f684f
dhintz <dhintz89@gmail.com>
2020-08-25 19:32:35 -0400

    removed logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,39 +10,30 @@ class ApplicationController < ActionController::API
     end
     
     def decoded_token
-        binding.pry
         if auth_header
             token = auth_header.split(' ')[1]
-            puts "decoded_token function: token: #{token}"
             begin
                 return JWT.decode(token, ENV["JWT_Secret"])
-                puts "decoded_token function: decoded_token: #{JWT.decode(token, ENV["JWT_Secret"])}"
             rescue JWT::DecodeError
-                nil
+                return nil
             end
         end
     end
 
     def current_user
-        puts "current_user function 1"
-        puts "#{decoded_token}"
-        puts "decoded_token if: #{!!decoded_token}"
         if decoded_token
-            puts "current_user function 2"
             user_id = decoded_token[0]['user_id']
-            puts "current_user function: user_id: #{user_id}; "
             @user = User.find_by(id: user_id)
-            puts "current_user function: @user: #{@user}"
+            puts @user
+            return @user
         end
     end
 
     def logged_in?
         !!current_user
-        puts "logged_in?: #{!!current_user}"
     end
 
     def authorized
-        puts "authorized function: is someone logged in: #{logged_in?}"
         render json: {error: 'You must log in to perform that action'}, status: :unauthorized unless logged_in?
     end
 
